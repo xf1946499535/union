c457116d21888010bdc14e2ae5b6a3d4b9fce8e0
Bobhellenofchina <1985493901@qq.com>
2021-09-18 22:58:43 +0800

    【feature】java后台搭建

---
+++ serve_code/union_java/src/main/java/com/zhj/UnionJavaApplication.java
@@ -0,0 +1,13 @@
+package com.zhj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnionJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnionJavaApplication.class, args);
+    }
+
+}
---
+++ serve_code/union_java/src/test/java/com/example/union_java/UnionJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.union_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnionJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
