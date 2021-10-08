4c45c35d239a5600498eaf03e1a4d33b9ac8b7b8
xiangfeng <xiangfeng@qianxin.com>
2021-10-08 17:01:53 +0800

    【feature】登录页面样式修改，加入粒子效果

--- vue_code/wo_web/src/main.js
+++ vue_code/wo_web/src/main.js
@@ -9,7 +9,10 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
+import VueParticles from 'vue-particles' 
 
+
+Vue.use(VueParticles)  
 Vue.use(VueQuillEditor)
 Vue.config.productionTip = false
 Vue.use(ElementUI);
